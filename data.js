7014d0a79961e6e2ffc67ed2f02fa828c677cbc0
Mateus Alves <48990862+Selbstverlust@users.noreply.github.com>
2024-12-27 16:10:18 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,414 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada",
+      "name": "Entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.00444109984565344,
+          "pitch": 0.17150157436876334,
+          "rotation": 0,
+          "target": "1-deck"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-deck",
+      "name": "Deck",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8714136844534828,
+          "pitch": 0.2214889999477947,
+          "rotation": 0,
+          "target": "4-produtos-naturais"
+        },
+        {
+          "yaw": 1.2226332554248405,
+          "pitch": 0.23468240615214064,
+          "rotation": 0,
+          "target": "6-stand"
+        },
+        {
+          "yaw": 0.009257944344202684,
+          "pitch": 0.2397145169680659,
+          "rotation": 0,
+          "target": "2-academia"
+        },
+        {
+          "yaw": -1.1226548758923656,
+          "pitch": 0.22292688554580486,
+          "rotation": 0,
+          "target": "3-remdios-naturais"
+        },
+        {
+          "yaw": -1.987172392367004,
+          "pitch": 0.24395343484376575,
+          "rotation": 0,
+          "target": "5-auditrio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-academia",
+      "name": "Academia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.120481464154839,
+          "pitch": 0.42142655937892926,
+          "rotation": 0,
+          "target": "1-deck"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.011102436258708437,
+          "pitch": -0.11714429471192034,
+          "title": "Treino VIDA10<br>",
+          "text": "<div>Text</div>"
+        },
+        {
+          "yaw": -0.23957276744613054,
+          "pitch": 0.15829374726250656,
+          "title": "<div>Alongamento</div>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.003455298734589718,
+          "pitch": 0.12608248656145093,
+          "title": "Aeróbico Intervalado<br>",
+          "text": "<div>Text<br></div>"
+        },
+        {
+          "yaw": 0.24254804077814995,
+          "pitch": 0.16850986806171164,
+          "title": "Sensório Motor<br>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-remdios-naturais",
+      "name": "Remédios Naturais",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.11993581018545,
+          "pitch": 0.19774433826000148,
+          "rotation": 0,
+          "target": "1-deck"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1168013309116827,
+          "pitch": 0.15987128051087396,
+          "title": "<div>Ar Puro</div>",
+          "text": "Text",
+          "pdfPath": "pdf/Rémedios Naturais - Ar Puro.pdf"
+        },
+        {
+          "yaw": -0.7821797423765275,
+          "pitch": 0.16175896947359725,
+          "title": "Água<br>",
+          "text": "Text",
+          "pdfPath": "pdf/Rémedios Naturais - Água.pdf"
+        },
+        {
+          "yaw": -0.46836480626305743,
+          "pitch": 0.15984848113371086,
+          "title": "Luz Solar<br>",
+          "text": "Text",
+          "pdfPath": "pdf/Rémedios Naturais - Luz Solar.pdf"
+        },
+        {
+          "yaw": -0.14865705805616614,
+          "pitch": 0.1566591995808171,
+          "title": "Exercício",
+          "text": "Text",
+          "pdfPath": "pdf/Rémedios Naturais - Exercícios Físicos.pdf"
+        },
+        {
+          "yaw": 0.1576188879545235,
+          "pitch": 0.15726460134160725,
+          "title": "Nutrição",
+          "text": "Text",
+          "pdfPath": "pdf/Rémedios Naturais - Nutrição.pdf"
+        },
+        {
+          "yaw": 0.46819219128707346,
+          "pitch": 0.16296751937791676,
+          "title": "Descanso",
+          "text": "Text",
+          "pdfPath": "pdf/Rémedios Naturais - Descanso.pdf"
+        },
+        {
+          "yaw": 0.7945453953925679,
+          "pitch": 0.1698205408479101,
+          "title": "Equilíbrio",
+          "text": "Text",
+          "pdfPath": "pdf/Rémedios Naturais - Temperança.pdf"
+        },
+        {
+          "yaw": 1.1222134305549716,
+          "pitch": 0.1705068468927191,
+          "title": "Confiança",
+          "text": "Text",
+          "pdfPath": "pdf/Rémedios Naturais - Confiança.pdf"
+        }
+      ]
+    },
+    {
+      "id": "4-produtos-naturais",
+      "name": "Produtos Naturais",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.909698688959544,
+          "pitch": 0.29041304563695647,
+          "rotation": 0,
+          "target": "1-deck"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-auditrio",
+      "name": "Auditório",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.125682528884493,
+          "pitch": 0.3185715006169154,
+          "rotation": 0,
+          "target": "1-deck"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-stand",
+      "name": "Stand",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.14092641966009,
+          "pitch": 0.21877782684101632,
+          "rotation": 0,
+          "target": "1-deck"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6236731440055703,
+          "pitch": 0.04503270140616067,
+          "title": "Racional",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.32066728041468373,
+          "pitch": 0.04297386682413418,
+          "title": "Emocional",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.005377270274848911,
+          "pitch": 0.039627272979341654,
+          "title": "Físico",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.3100754044634204,
+          "pitch": 0.042506396497170584,
+          "title": "Nutricional",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.6289795944923959,
+          "pitch": 0.04591282592515,
+          "title": "Espiritual",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "VIDA10",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
