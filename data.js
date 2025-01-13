b37a115e55264821576d0321fe53d1b6c9a7c515
Mateus Alves <48990862+Selbstverlust@users.noreply.github.com>
2025-01-13 15:53:05 -0300

    Alterações na tela de Rémedios Naturais para Hotspots de modal. Links de videos adicionados.

--- data.js
+++ data.js
@@ -203,58 +203,50 @@ var APP_DATA = {
         {
           "yaw": -1.1168013309116827,
           "pitch": 0.15987128051087396,
-          "title": "<div>Ar Puro</div>",
-          "text": "Text",
-          "pdfPath": "pdf/Rémedios Naturais - Ar Puro.pdf"
+          "title": "Remédios Naturais - Ar Puro",
+          "text": "<div class='pdf-links'><a href='pdf/Rémedios Naturais - Ar Puro.pdf' target='_blank'>Abrir PDF - Ar Puro</a><br><a href='https://www.youtube.com/watch?v=gnhiKO84vaU' target='_blank'>Abrir Vídeo 1 - Ar Puro</a><br><a href='https://www.youtube.com/watch?v=uP0v52-0DIM' target='_blank'>Abrir Vídeo 2 - Ar Puro</a></div>"
         },
         {
           "yaw": -0.7821797423765275,
           "pitch": 0.16175896947359725,
-          "title": "Água<br>",
-          "text": "Text",
-          "pdfPath": "pdf/Rémedios Naturais - Água.pdf"
+          "title": "Remédios Naturais - Água",
+          "text": "<div class='pdf-links'><a href='pdf/Rémedios Naturais - Água.pdf' target='_blank'>Abrir PDF - Água</a><br><a href='https://www.youtube.com/watch?v=P2B4MGinc3U' target='_blank'>Abrir Vídeo 1 - Água</a><br><a href='https://www.youtube.com/watch?v=nl1ZJU8QWFM' target='_blank'>Abrir Vídeo 2 - Água</a></div>"
         },
         {
           "yaw": -0.46836480626305743,
           "pitch": 0.15984848113371086,
-          "title": "Luz Solar<br>",
-          "text": "Text",
-          "pdfPath": "pdf/Rémedios Naturais - Luz Solar.pdf"
+          "title": "Remédios Naturais - Luz Solar",
+          "text": "<div class='pdf-links'><a href='pdf/Rémedios Naturais - Luz Solar.pdf' target='_blank'>Abrir PDF - Luz Solar</a><br><a href='https://www.youtube.com/watch?v=Nc2HRtK5bBs' target='_blank'>Abrir Vídeo 1 - Luz Solar</a><br><a href='https://www.youtube.com/watch?v=4C7eeG8iUL8' target='_blank'>Abrir Vídeo 2 - Luz Solar</a></div>"
         },
         {
           "yaw": -0.14865705805616614,
           "pitch": 0.1566591995808171,
-          "title": "Exercício",
-          "text": "Text",
-          "pdfPath": "pdf/Rémedios Naturais - Exercícios Físicos.pdf"
+          "title": "Remédios Naturais - Exercício",
+          "text": "<div class='pdf-links'><a href='pdf/Rémedios Naturais - Exercícios Físicos.pdf' target='_blank'>Abrir PDF - Exercícios Físicos</a><br><a href='https://www.youtube.com/watch?v=S5xPI4hZlQw' target='_blank'>Abrir Vídeo 1 - Exercícios Físicos</a><br><a href='https://www.youtube.com/watch?v=hz1mdb3cTgs' target='_blank'>Abrir Vídeo 2 - Exercícios Físicos</a></div>"
         },
         {
           "yaw": 0.1576188879545235,
           "pitch": 0.15726460134160725,
-          "title": "Nutrição",
-          "text": "Text",
-          "pdfPath": "pdf/Rémedios Naturais - Nutrição.pdf"
+          "title": "Remédios Naturais - Nutrição",
+          "text": "<div class='pdf-links'><a href='pdf/Rémedios Naturais - Nutrição.pdf' target='_blank'>Abrir PDF - Nutrição</a><br><a href='https://www.youtube.com/watch?v=KypN4Y2AyLk' target='_blank'>Abrir Vídeo 1 - Nutrição</a><br><a href='https://www.youtube.com/watch?v=XtxabkLn_ic' target='_blank'>Abrir Vídeo 2 - Nutrição</a></div>"
         },
         {
           "yaw": 0.46819219128707346,
           "pitch": 0.16296751937791676,
-          "title": "Descanso",
-          "text": "Text",
-          "pdfPath": "pdf/Rémedios Naturais - Descanso.pdf"
+          "title": "Remédios Naturais - Descanso",
+          "text": "<div class='pdf-links'><a href='pdf/Rémedios Naturais - Descanso.pdf' target='_blank'>Abrir PDF - Descanso</a><br><a href='https://www.youtube.com/watch?v=uUMB4wVf6m4' target='_blank'>Abrir Vídeo 1 - Descanso</a><br><a href='https://www.youtube.com/watch?v=BPDtFcA_1Is' target='_blank'>Abrir Vídeo 2 - Descanso</a></div>"
         },
         {
           "yaw": 0.7945453953925679,
           "pitch": 0.1698205408479101,
-          "title": "Equilíbrio",
-          "text": "Text",
-          "pdfPath": "pdf/Rémedios Naturais - Temperança.pdf"
+          "title": "Remédios Naturais - Equilíbrio",
+          "text": "<div class='pdf-links'><a href='pdf/Rémedios Naturais - Temperança.pdf' target='_blank'>Abrir PDF - Equilíbrio</a><br><a href='https://www.youtube.com/watch?v=hqE6P6KMlrM' target='_blank'>Abrir Vídeo 1 - Equilíbrio</a><br><a href='https://www.youtube.com/watch?v=YSO21au6rfc' target='_blank'>Abrir Vídeo 2 - Equilíbrio</a></div>"
         },
         {
           "yaw": 1.1222134305549716,
           "pitch": 0.1705068468927191,
-          "title": "Confiança",
-          "text": "Text",
-          "pdfPath": "pdf/Rémedios Naturais - Confiança.pdf"
+          "title": "Remédios Naturais - Confiança",
+          "text": "<div class='pdf-links'><a href='pdf/Rémedios Naturais - Confiança.pdf' target='_blank'>Abrir PDF - Confiança</a><br><a href='https://www.youtube.com/watch?v=47msFQlOITs' target='_blank'>Abrir Vídeo 1 - Confiança</a><br><a href='https://www.youtube.com/watch?v=bhj75dPKIMw' target='_blank'>Abrir Vídeo 2 - Confiança</a></div>"
         }
       ]
     },
